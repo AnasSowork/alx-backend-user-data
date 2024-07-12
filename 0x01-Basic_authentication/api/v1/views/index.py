7ab3a20aa7e61e40611c17ba8065a5b2f1ca79a5
Anas <anas.001falaki@gmail.com>
2024-07-12 23:32:49 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,17 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized():
+    """ Abort request with error code 401
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden():
+    """ Abort request with error code 403
+    """
+    abort(403)
